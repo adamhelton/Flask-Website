4eb6c3fc24e03348f1683567c05c7d933fb63c09
Adam Helton <helton93@protonmail.com>
2019-06-16 17:57:03 -0400

    Add project files.

---
+++ Website/app.py
@@ -0,0 +1,30 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+
+@app.route('/')
+def home():
+    """Renders a sample page."""
+    return "The Home Page will go here"
+
+@app.route('/about/')
+def about():
+    return "The About Content will go here"
+    
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
